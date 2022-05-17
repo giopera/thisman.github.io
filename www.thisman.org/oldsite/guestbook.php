b5f9b8af2832fc6c7ddf32b20c244917e151ffa4
Lapa_Pera (Git Edition) <89829281+GioPera06@users.noreply.github.com>
2022-05-17 14:52:02 +0200

    Initial Commit

---
+++ www.thisman.org/oldsite/guestbook.php
@@ -0,0 +1,41 @@
+<body bgcolor="#FFFFFF" text="#000000" link="#000000" vlink="#000000" alink="#000000">
+
+<div id="spot-im-root"></div><script>!function(t,e,o){function p(){var t=e.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==e.location.protocol?"https":"http")+":"+o,e.body.appendChild(t)}t.spotId="00d123552b1a0e5de49143ca4b5a3721",t.spotName="",t.allowDesktop=!0,t.allowMobile=!1,t.containerId="spot-im-root",p()}(window.SPOTIM={},document,"//www.spot.im/embed/scripts/launcher.js");</script>
+
+
+<div id="fb-root"></div>
+
+
+<script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/it_IT/all.js#xfbml=1&appId=64083750024";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>                    
+
+<div align="center"><font face="Arial,Helvetica,sans-serif" size="7"><strong>EVER DREAM THIS MAN?</strong><br><font size="5">every night throughout the world hundreds of people dream about this face</font></font><br></div><br><br><div align="center"><table border="0" cellspacing="2" cellpadding="2"><tr>    <td width="100"></td>    <td width="200" valign="top"><font face="Arial,Helvetica,sans-serif" size="6">	<br>	<a href="./history.htm">History</a><br>	<a href="./dreams.htm">Dreams</a><br>	<a href="./theories.htm">Theories</a><br>	<a href="./portraits.htm">Portraits</a><br>		<a href="./intheworld.htm">In the world</a><br>	<a href="./download.htm">Download</a><br>		<a href="./guestbook.php">Guestbook</a><br>	<a href="./news.htm">News</a><br>	<a href="javascript:apri('contact.htm');">Contact us</a><br><br>	<a href="index.htm"><img src="thisman_small.jpg" alt="" width="200" height="234" border="0"></a><br><br><br>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-7828832081148016";
+/* thisman - laterale */
+google_ad_slot = "2033460965";
+google_ad_width = 200;
+google_ad_height = 200;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+
+</td>		<td width="100"></td>    <td width="500">		<font face="Arial,Helvetica,sans-serif"><div class="fb-like-box" data-href="https://www.facebook.com/pages/Ever-Dream-This-Man/370348283088502?skip_nax_wizard=true&amp;success=1#" data-width="450" data-show-faces="true" data-stream="true" data-show-border="false" data-header="true"></div>		<td width="100"></td></tr><tr>    <td colspan="5"><div align="center"><br><br><br>	<font face="Arial,Helvetica,sans-serif" size="1">e-mail: <a href="mailto:info@thisman.org">info@thisman.org</a>	</font></div>	</td></tr></table></div><br></font></div><div align="center"></div><div align="center">
+
+<script type="text/javascript"><!--
+google_ad_client = "pub-7828832081148016";
+/* 728x90, created 10/16/09 */
+google_ad_slot = "4261748011";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+</div>
+</body>
