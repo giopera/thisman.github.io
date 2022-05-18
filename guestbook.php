fd4368a4552733603fae6734df0def8f5692a1e8
murphyxy <105114462+murphyxy@users.noreply.github.com>
2022-05-18 15:19:22 +0200

    Traduzioni divine
    gentilmente fornite da BRZCLD

--- guestbook.php
+++ guestbook.php
@@ -10,7 +10,7 @@
 
 <div align="center"><font face="Arial,Helvetica,sans-serif" size="7"><strong>EVER DREAM THIS MAN?</strong><br><font size="5">every night throughout the world hundreds of people dream about this face</font></font><br></div><br><br><div align="center"><table border="0" cellspacing="2" cellpadding="2"><tr>    <td width="100"></td>    <td width="200" valign="top"><font face="Arial,Helvetica,sans-serif" size="6">	<br>	<a href="./history.htm">History</a><br>	<a href="./dreams.htm">Dreams</a><br>	<a href="./theories.htm">Theories</a><br>	<a href="./portraits.htm">Portraits</a><br>		<a href="./intheworld.htm">In the world</a><br>	<a href="./download.htm">Download</a><br>		<a href="./guestbook.php">Guestbook</a><br>	<a href="./news.htm">News</a><br>	<a href="javascript:apri('contact.htm');">Contact us</a><br><br>	<a href="index.htm"><img src="thisman_small.jpg" alt="" width="200" height="234" border="0"></a><br><br><br>
 
-<script type="text/javascript"><!--
+<script type="text/javascript">
 google_ad_client = "ca-pub-7828832081148016";
 /* thisman - laterale */
 google_ad_slot = "2033460965";
@@ -25,7 +25,7 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 
 </td>		<td width="100"></td>    <td width="500">		<font face="Arial,Helvetica,sans-serif"><div class="fb-like-box" data-href="https://www.facebook.com/pages/Ever-Dream-This-Man/370348283088502?skip_nax_wizard=true&amp;success=1#" data-width="450" data-show-faces="true" data-stream="true" data-show-border="false" data-header="true"></div>		<td width="100"></td></tr><tr>    <td colspan="5"><div align="center"><br><br><br>	<font face="Arial,Helvetica,sans-serif" size="1">e-mail: <a href="mailto:info@thisman.org">info@thisman.org</a>	</font></div>	</td></tr></table></div><br></font></div><div align="center"></div><div align="center">
 
-<script type="text/javascript"><!--
+<script type="text/javascript">
 google_ad_client = "pub-7828832081148016";
 /* 728x90, created 10/16/09 */
 google_ad_slot = "4261748011";
